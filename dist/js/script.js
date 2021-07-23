c82e7b84f72b0e5ac594d90b2ce3d26ea65879c6
KubanovValentin <delo.biss13@gmail.com>
2021-07-23 21:46:47 +0300

    making up the main page

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,31 +1,52 @@
-function myFunction() {
-    // Объявлять переменные
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById('myInput');
-    filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName('li');
 
-    // Выполните цикл по всем элементам списка и скройте те, которые не соответствуют запросу поиска
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
-        }
-    }
-}
 
-$(document).ready(function(){
-    $('.next').click(function() {
-        $('.pagination').find('.pageNumber.active').next().addClass('active');
-        $('.pagination').find('.pageNumber.active').prev().removeClass('active');
+const menu = document.querySelector('.menu'),
+        menuItem = document.querySelectorAll('.menu__item'),
+        hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
     });
-    $('.next').click(function() {
-        $('.pagination').find('.pageNumber.active').prev().addClass('active');
-        $('.pagination').find('.pageNumber.active').next().removeClass('active');
-    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        });
 });
+
+
+
+
+//function myFunction() {
+//     // Объявлять переменные
+//     var input, filter, ul, li, a, i, txtValue;
+//     input = document.getElementById('myInput');
+//     filter = input.value.toUpperCase();
+//     ul = document.getElementById("myUL");
+//     li = ul.getElementsByTagName('li');
+
+//     // Выполните цикл по всем элементам списка и скройте те, которые не соответствуют запросу поиска
+//     for (i = 0; i < li.length; i++) {
+//         a = li[i].getElementsByTagName("a")[0];
+//         txtValue = a.textContent || a.innerText;
+//         if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//             li[i].style.display = "";
+//         } else {
+//             li[i].style.display = "none";
+//         }
+//     }
+// }
+
+// $(document).ready(function(){
+//     $('.next').click(function() {
+//         $('.pagination').find('.pageNumber.active').next().addClass('active');
+//         $('.pagination').find('.pageNumber.active').prev().removeClass('active');
+//     });
+//     $('.next').click(function() {
+//         $('.pagination').find('.pageNumber.active').prev().addClass('active');
+//         $('.pagination').find('.pageNumber.active').next().removeClass('active');
+//     });
+// });
 
